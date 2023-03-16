7c6640ced664bb33694b93221aaf243ed04e70d2
Nikita5152 <114350496+Nikita5152@users.noreply.github.com>
2023-03-16 17:53:04 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.baseDataSetTableAdapters;
+
+namespace WpfApp1
+{
+
+    public partial class MainWindow : Window
+    {
+        HumanTableAdapter sss = new HumanTableAdapter();
+        TovarTableAdapter aaa = new TovarTableAdapter();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            HumanDataGrid.ItemsSource = sss.GetData();
+            TovarComboBox.ItemsSource = aaa.GetData();
+            TovarComboBox.DisplayMemberPath = "Title";
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object cell = (TovarComboBox.SelectedItem as DataRowView).Row[1];
+            MessageBox.Show(cell.ToString());
+        }
+    }
+}
